2929191edb5815a2c95f29cba556bc368e6bceb4
Orland <51851150+OrlandV@users.noreply.github.com>
2024-05-11 20:20:22 +0300

    3.Factorial.py

--- 3.Factorial.py
+++ 3.Factorial.py
@@ -1,9 +1,22 @@
+# Функция с произвольным числом параметров разного типа.
+def test(*args, **kwargs):
+    i = 0
+    for v in args:
+        print(f'{i}: {v}')
+        i += 1
+    for k, v in kwargs.items():
+        print(f'{k}: {v}')
+
+
 def factorial(n):
     if n == 1 or n == 0:
         return 1
     elif n > 1:
         return n * factorial(n - 1)
 
+
+# Вызов функции с произвольным числом параметров разного типа.
+test('r', {'f': 15, 'y': 'h'}, l=[2, 't', (4, 7)], d={1: 5})
 
 num = ''
 while num != 'стоп':
