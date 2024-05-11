fb3e9f8a337c7b6d594fa66b0b90ca645ec220c0
Orland <51851150+OrlandV@users.noreply.github.com>
2024-05-11 16:37:46 +0300

    3.Factorial.py

--- 3.Factorial.py
+++ 3.Factorial.py
@@ -1,5 +1,5 @@
 def factorial(n):
-    if n == 1:
+    if n == 1 or n == 0:
         return 1
     elif n > 1:
         return n * factorial(n - 1)
@@ -10,10 +10,7 @@ while num != 'стоп':
     num = input('Введите целое положительное число для расчёта его факториала или «стоп» для выхода: ')
     if num.isdigit():
         num = int(num)
-        if num == 0:
-            print(f'Ошибка ввода. Введено не положительное число ({num}). 0! = 1')
-        else:
-            print(factorial(num))
+        print(f'{num}! = ', factorial(num))
     elif num != 'стоп':
         print('Ошибка ввода. Введено не целое положительное число.')
 print('Завершение программы.')
